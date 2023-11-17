c5d68403def4f70cf14ecf6925423bb8d3686637
Kirsten Groenewald <143618284+KHGroenie@users.noreply.github.com>
2023-11-17 09:55:15 +0200

    nasa files

---
+++ script.js
@@ -0,0 +1,162 @@
+const resultsNav = document.getElementById('resultsNav');
+const favouritesNav = document.getElementById('favouritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
+// NASA API
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+let favourites = {};
+
+// Scroll To Top, Remove Loader, Show Content
+function showContent(page) {
+    window.scrollTo({ top: 0, behavior: 'instant' });
+    loader.classList.add('hidden');
+    if (page === 'results') {
+        resultsNav.classList.remove('hidden');
+        favouritesNav.classList.add('hidden');
+    } else {
+        resultsNav.classList.add('hidden');
+        favouritesNav.classList.remove('hidden');
+    }
+}
+
+function createDOMNodes(page) {
+    // Load ResultsArray or Favorites
+    const currentArray = page === 'results' ? resultsArray : Object.values(favourites);
+    currentArray.forEach((result) => {
+        // Card Container
+        const card = document.createElement('div');
+        card.classList.add('card');
+        // Link
+        const link = document.createElement('a');
+        link.href = result.hdurl;
+        link.title = 'View Full Image';
+        link.target = '_blank';
+        // Image
+        const image = document.createElement('img');
+        image.src = result.url;
+        image.alt = 'NASA Picture of the Day';
+        image.loading = 'lazy';
+        image.classList.add('card-img-top');
+        // Card Body
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        // Card Title
+        const cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = result.title;
+        // Save Text
+        const saveText = document.createElement('p');
+        saveText.classList.add('clickable');
+        if (page === 'results') {
+            saveText.textContent = 'Add To favourites';
+            saveText.setAttribute('onclick', `saveFavourite('${result.url}')`);
+        } else {
+            saveText.textContent = 'Remove Favourite';
+            saveText.setAttribute('onclick', `removeFavourite('${result.url}')`);
+        }
+        // Card Text
+        const cardText = document.createElement('p');
+        cardText.textContent = result.explanation;
+        // Footer Container
+        const footer = document.createElement('small');
+        footer.classList.add('text-muted');
+        // Date
+        const date = document.createElement('strong');
+        date.textContent = result.date;
+        // Copyright
+        const copyrightResult = result.copyright === undefined ? '' : result.copyright;
+        const copyright = document.createElement('span');
+        copyright.textContent = ` ${copyrightResult}`;
+        // Append
+        footer.append(date, copyright);
+        cardBody.append(cardTitle, saveText, cardText, footer);
+        link.appendChild(image);
+        card.append(link, cardBody);
+        imagesContainer.appendChild(card);
+    });
+}
+
+function updateDOM(page) {
+    // Get favourites from localStorage
+    if (localStorage.getItem('nasafavourites')) {
+        favourites = JSON.parse(localStorage.getItem('nasafavourites'));
+    }
+    // Reset DOM, Create DOM Nodes, Show Content
+    imagesContainer.textContent = '';
+    createDOMNodes(page);
+    showContent(page);
+}
+
+// Get 10 images from NASA API
+async function getNasaPictures() {
+    // Show Loader
+    loader.classList.remove('hidden');
+    try {
+        const response = await fetch(apiUrl);
+        resultsArray = await response.json();
+        updateDOM('results');
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// Add result to favourites
+function saveFavourite(itemUrl) {
+    // Loop through Results Array to select Favorite
+    resultsArray.forEach((item) => {
+        if (item.url.includes(itemUrl) && !favourites[itemUrl]) {
+            favourites[itemUrl] = item;
+            // Show Save Confirmation for 2 seconds
+            saveConfirmed.hidden = false;
+            setTimeout(() => {
+                saveConfirmed.hidden = true;
+            }, 2000);
+            // Set favourites in localStorage
+            localStorage.setItem('nasafavourites', JSON.stringify(favourites));
+        }
+    });
+}
+
+// Remove item from favourites
+function removeFavourite(itemUrl) {
+    if (favourites[itemUrl]) {
+        delete favourites[itemUrl];
+        // Set favourites in localStorage
+        localStorage.setItem('nasafavourites', JSON.stringify(favourites));
+        updateDOM('favourites');
+    }
+}
+
+// On Load
+getNasaPictures();
+const { body } = document;
+
+function changeBackground(number) {
+    // Check if background already showing
+    let previousBackground;
+    if (body.className) {
+        previousBackground = body.className;
+    }
+    // Reset CSS class for body
+    body.className = ''
+    switch (number) {
+        case '1':
+            return (previousBackground === 'background-1' ? false : body.classList.add('background-1'));
+        case '2':
+            return (previousBackground === 'background-2' ? false : body.classList.add('background-2'));
+
+        case '3':
+            return (previousBackground === 'background-3' ? false : body.classList.add('background-3'));
+        case '4':
+            return (previousBackground === 'background-4' ? false : body.classList.add('background-4'));
+
+        default:
+            break;
+    }
+}
